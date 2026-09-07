8ff0338a572fdd6b651373b87ee93c7f9ded11ad
Aditya Parag Singh <adityaparagsingh@gmail.com>
2026-09-07 14:06:39 +0530

    Time: 59 ms (5.02%), Space: 19.2 MB (64.31%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,5 +1,5 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         num1 = format(n,'032b')
-        num2 = int(str(num1)[::-1],2)
-        return num2
+        # num2 = int(str(num1)[::-1],2)
+        return int(str(num1)[::-1],2)
