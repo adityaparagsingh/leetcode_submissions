de9dae0dc5e33701ff00a92a72c1464b3fe8171c
Aditya Parag Singh <adityaparagsingh@gmail.com>
2026-09-13 16:02:12 +0530

    Time: 0 ms (100%), Space: 19.4 MB (18.83%) - LeetHub

--- 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.py
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.py
@@ -1,5 +1,6 @@
 class Solution:
     def addedInteger(self, nums1: List[int], nums2: List[int]) -> int:
-        nums1.sort()
-        nums2.sort()
-        return nums2[0]-nums1[0]
+        # nums1.sort()
+        # nums2.sort()
+        # return nums2[0]-nums1[0]
+        return max(nums2)-max(nums1)
